8f1b15e42289b8010fbe77b87e00d86589a23ff0
Bjorn <bjorn@ambientchill.com>
2025-07-02 21:25:02 -0700

    add nvim-tree

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
+-- for nvim-tree, disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
 require("config.lazy")
 
 vim.lsp.enable('clangd')
+-- lsp
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error' })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
-
-print("Hello!")
+-- nvim-tree
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
