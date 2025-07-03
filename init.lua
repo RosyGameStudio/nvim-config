8678b9599a4145ad13633010475ff8c04b3f649e
Bjorn <bjorn@ambientchill.com>
2025-07-02 23:16:04 -0700

    moar docs

--- init.lua
+++ init.lua
@@ -17,3 +17,13 @@ vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { desc = 'Toggle file ex
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 vim.keymap.set('n', '<leader>th', ':split | terminal<CR>', { desc = 'Open terminal in horizontal split' })
 vim.keymap.set('n', '<leader>tv', ':vsplit | terminal<CR>', { desc = 'Open terminal in vertical split' })
+--build & debug
+vim.keymap.set('n', '<F5>', ':!build.bat<CR>', { desc = 'Run build.bat' })
+vim.keymap.set('n', '<S-F5>', ':!debug.bat<CR>', { desc = 'Run debug.bat' })
+-- docs
+vim.keymap.set('n', '<leader>hc', function()
+  vim.fn.system('start https://learn.microsoft.com/en-us/cpp/c-language/?view=msvc-170')
+end, { desc = 'Open C language docs' })
+vim.keymap.set('n', '<leader>hv', function()
+  vim.fn.system('start https://registry.khronos.org/vulkan/specs/latest/html/vkspec.html')
+end, { desc = 'Open C language docs' })
