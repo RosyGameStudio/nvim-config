aff3aabd2bb7e3888cb586411a95977b8982346d
Bjorn <bjorn@ambientchill.com>
2025-07-26 14:04:54 -0700

    updates

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-pack/nvim-spectre",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  config = function()
+    require('spectre').setup()
+  end,
+  keys = {
+    { "<leader>ss", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre" },
+    { "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word" },
+    { "<leader>sv", '<cmd>lua require("spectre").open_visual()<CR>', desc = "Search current word" },
+    { "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Search on current file" },
+  }
+}
